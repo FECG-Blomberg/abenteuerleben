495957d6a0e00689efe324debbe1fcb40a5a6ca8
- <44647308+anosim114@users.noreply.github.com>
2024-06-11 18:05:10 +0200

    Add open in whatsapp link to parent

--- test/models/parent_test.rb
+++ test/models/parent_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class ParentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test  "telephone to whatsapp format works just fine" do
+    cases = [
+      "+49 12 34567",
+      "+491234567",
+      "491 234567",
+      "491234567",
+      "01234567",
+      "0 123 4567",
+    ]
+
+    cases.each do |number|
+      # prepare
+      p = Parent::Parent.new
+      p.telephone = number
+
+      # execute
+      formatted_number = p.wa_telephone
+
+      # check
+      assert_equal "491234567", formatted_number
+    end
+  end
+
+  test  "telephone to whatsapp format works just fine for non-german numbers" do
+    # prepare
+    p = Parent::Parent.new
+    p.telephone = "+22 123 4567"
+
+    # execute
+    formatted_number = p.wa_telephone
+
+    # check
+    assert_equal "221234567", formatted_number
+  end
 end
