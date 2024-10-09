03fbd41711792b264ca6ba7db19b10c4c1cccb06
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-10-09 17:10:30 +0200

    Try to fix pipeline

--- test/test_helpers/cuprite_setup.rb
+++ test/test_helpers/cuprite_setup.rb
@@ -5,11 +5,15 @@ require "capybara/cuprite"
 # NOTE: The name :cuprite is already registered by Rails.
 # See https://github.com/rubycdp/cuprite/issues/180
 Capybara.register_driver(:better_cuprite) do |app|
+  browser_options = {}.tap do |opts|
+    opts['no-sandbox'] = nil if ENV['CI']
+  end
+
   Capybara::Cuprite::Driver.new(
     app,
     **{
       window_size: [1200, 800],
-      browser_options: {},
+      browser_options: ENV['CI'] ? {'no-sandbox': nil} : {},
       process_timeout: 10,
       inspector: true,
       headless: ENV.fetch("CI", false)
