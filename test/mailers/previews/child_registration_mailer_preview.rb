bcb5263237f8f668844e2786628176f44dd13a7f
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-10-10 21:26:03 +0200

    Update email numbers (#210)
    * Update email template
    
    Fix some small formatting issues
    
    * Update helper mail tempaltes
    
    * Update chile email template
    
    * Add galaxy start page
    
    * Fix turbo breaking notice on home page
    
    * Fix turbo breaking notice on home page

--- test/mailers/previews/child_registration_mailer_preview.rb
+++ test/mailers/previews/child_registration_mailer_preview.rb
@@ -1,8 +1,34 @@
 # Preview all emails at http://localhost:3000/rails/mailers/child_registration_mailer
 class ChildRegistrationMailerPreview < ActionMailer::Preview
   def registered_mail
-    parent = Parent::Parent.first!
-    child = parent.children[0]
+    parent = Parent::Parent.new
+    parent.surname = 'Musterman'
+    parent.forename = 'Max'
+    parent.telephone = '01234567'
+    parent.housephone = '0223456'
+    parent.email = 'max_musterman@example.com'
+    parent.street = 'musterstr.'
+    parent.house = '111'
+    parent.post = '12345'
+    parent.city = 'musterstadt'
+    parent.post = 'false'
+    parent.church = ''
+    child = Child.new
+    child.surname = 'Musterman'
+    child.forename = 'Asdfine'
+    child.birthday = '2000-01-01'
+    child.sex = 'maedchen'
+    child.medicals = 'nothing'
+    child.notes = 'also nothing'
+    child.wishmate = 'The one from family B'
+    camp = Camp.new
+    camp.name = "One"
+    camp.price = '123 EURO'
+    camp.date_start = '2020-01-01'
+    camp.date_end = '2020-01-02'
+
+    child.camp = camp
+    parent.children << child
 
     ChildRegistrationMailer.with(parent: parent, child: child).registered_mail
   end
