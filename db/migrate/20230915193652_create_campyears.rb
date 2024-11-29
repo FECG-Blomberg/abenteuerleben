ae996584a43147de40c6a041dd426206573fff44
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-11-29 11:24:22 +0100

    revert migration file changes
    better not to change migration file :d

--- db/migrate/20230915193652_create_campyears.rb
+++ db/migrate/20230915193652_create_campyears.rb
@@ -6,6 +6,8 @@ class CreateCampyears < ActiveRecord::Migration[7.0]
       t.date :participants_register_end
       t.date :helper_register_start
       t.date :helper_register_end
+      t.string :accentcolor_primary
+      t.string :accentcolor_secondary
 
       t.timestamps
     end
