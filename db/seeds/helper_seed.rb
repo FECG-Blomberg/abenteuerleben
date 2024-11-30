df87c8039928e97150b16709b6601354947dc4df
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-11-30 15:20:56 +0100

    clean up seeds a bit (#231)
    * clean up seeds a bit
    
    * Clean up helper email
    
    * Clean up helper email
    
    * Improve helper mail structure

---
+++ db/seeds/helper_seed.rb
@@ -0,0 +1,31 @@
+module Seeds
+  class HelperSeed
+    def self.create_helper(camp_id)
+      helper = Helper.new
+      helper.photo.attach(io: Tempfile.new('helper one image.png'), filename: 'img.png')
+      helper.surname = 'hSurname'
+      helper.forename = 'hForename'
+      helper.birthday = '2000-01-01'
+      helper.birthplace = 'Helpercity Neighborhood'
+      helper.telephone = '012345 67890'
+      helper.email = 'helper@example.com'
+      helper.streethouse = 'helperstreet 1'
+      helper.postcity = '12345 helpercity'
+      helper.story = 'I was once a simple helper'
+      helper.duty = 'Helping my my sole purpose'
+      helper.preferredCamp = 'none particularly'
+      helper.registrations.push(new_helper_registration(camp_id))
+      helper.save!
+      helper
+    end
+
+    def self.new_helper_registration camp_id
+      Registration.new(
+        camp_id: camp_id,
+        wish_first: 'helperDutyOne',
+        wish_second: 'helperDutyTwo',
+        participate: true
+      )
+    end
+  end
+end
